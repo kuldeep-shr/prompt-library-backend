1ca9276211d00ee1d4357c12966e0adf1dfded98
Kuldeep Sharma <kuldeepsharma8988@gmail.com>
2025-08-21 02:34:32 +0530

    chore: swapped DB to SQLite — lightweight but mighty 🪶🗄️

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,17 +1,16 @@
 import { DataSource } from "typeorm";
 import { config } from "dotenv";
 import { User } from "./entities/user/User";
-import { ShortUrl } from "./entities/url/ShortUrl";
-import { UrlAccessLog } from "./entities/url/UrlAccessLog";
-import { KGSKey } from "./entities/url/Kgs";
+import { Prompt } from "./entities/prompt/Prompt";
+import { Category } from "./entities/prompt/Category";
+// import { KGSKey } from "./entities/url/Kgs";
 
 config();
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  entities: [User, ShortUrl, UrlAccessLog, KGSKey],
-  synchronize: true,
-  // logging: true,
-  // logger: "advanced-console",
+  type: "sqlite",
+  database: process.env.DATABASE_PATH || "prompt.library.sqlite", 
+  entities: [User,Prompt,Category],
+  synchronize: true, // ⚠️ auto-sync, turn off in production
+  logging: false,
 });
